e09e44cdefe6875016440a260446aef807f6e21a
AntonBlomquist <antonblomquist@live.dk>
2021-12-15 14:19:29 +0100

    Add project files.

---
+++ UDP Receiver/Measure.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDP_Receiver {
+    public class Measure {
+        public string Classroom { get; set; }
+        public double Temperature { get; set; }
+        public int Humidity { get; set; }
+
+        public Measure(string classroom, double temp, int hum) {
+            Classroom = classroom;
+            Temperature = temp;
+            Humidity = hum;
+        }
+
+        public override string ToString() {
+            return $"{Classroom} - {Temperature} - {Humidity}";
+        }
+    }
+}
---
+++ UDP Receiver/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+using System.Net;
+using System.Net.Http;
+using System.Net.Sockets;
+using System.Text;
+using System.Text.Json;
+
+namespace UDP_Receiver {
+    class Program {
+        private const int Port = 7000;
+        static void Main() {
+            IPEndPoint ipEndPoint = new IPEndPoint(IPAddress.Any, Port);
+            using (UdpClient socket = new UdpClient(ipEndPoint)) {
+                IPEndPoint remoteEndPoint = new IPEndPoint(0, 0);
+                HttpClient rest = new HttpClient();
+                while (true) {
+                    Console.WriteLine("Waiting for broadcast {0}", socket.Client.LocalEndPoint);
+                    byte[] datagramReceived = socket.Receive(ref remoteEndPoint);
+                    string receivedData = Encoding.UTF8.GetString(datagramReceived);
+                    string[] splitData = receivedData.Split(' ');
+                    string Classroom = "D3.06";
+                    if(receivedData != null) {
+                        Measure measure = new Measure(Classroom, double.Parse(splitData[0], new CultureInfo("en-UK")), Int16.Parse(splitData[1], new CultureInfo("en-UK")));
+                        StringContent content = new StringContent(JsonSerializer.Serialize(measure), Encoding.UTF8, "application/json");
+                        rest.PostAsync($"https://restmasterairflow.azurewebsites.net/api/Measure/{Classroom}/{splitData[0]}/{splitData[1]}", content);
+                        Console.WriteLine(measure);
+                    }
+                }
+            }
+        }
+    }
+}
